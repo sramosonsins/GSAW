d13caf0980273ca68ef99ed9ad130ba737c37ad3
sramosonsins <sebastian.ramos@cragenomica.es>
2020-06-07 09:11:56 +0200

    add license file

--- compile_GSAW.sh
+++ compile_GSAW.sh
@@ -5,6 +5,31 @@ mkdir $DIR/bin
 
 cd $DIR/bin
 
+if [ "$(uname)" == "Darwin" ]; then
+    function _wget() { curl "${1}" -o $(basename "${1}") ; };
+    alias wget='_wget'
+fi
+
+#zlib 1.2.11 installation (dependency)
+mkdir -p ./zlib
+wget http://zlib.net/zlib-1.2.11.tar.gz -P ./zlib
+tar -zxvf ./zlib/zlib-1.2.11.tar.gz -C ./zlib
+rm ./zlib/zlib-1.2.11.tar.gz
+cd ./zlib/zlib-1.2.11
+./configure
+make
+sudo make install
+
+#gsl installation (dependency)
+mkdir -p /tmp/gsl
+curl -o /tmp/gsl-2.2.tar.gz ftp://ftp.gnu.org/gnu/gsl/gsl-2.2.tar.gz -LOk
+tar -zxvf /tmp/gsl-2.2.tar.gz -C /tmp/gsl && \
+rm /tmp/gsl-2.2.tar.gz && \
+cd /tmp/gsl/gsl-2.2 && \
+./configure && \
+make && \
+sudo make install
+
 ## Download the required packages/softwares
 
 git clone https://github.com/CRAGENOMICA/mstatspop.git
